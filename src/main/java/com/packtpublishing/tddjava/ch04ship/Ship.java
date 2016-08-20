a1816426b8dea1895704fdc3d2d55d168fff30d7
jimmy.do0704 <jimmy.do0704@gmail.com>
2016-08-20 18:28:36 +0700

    first requirement

--- src/main/java/com/packtpublishing/tddjava/ch04ship/Ship.java
+++ src/main/java/com/packtpublishing/tddjava/ch04ship/Ship.java
@@ -2,4 +2,12 @@ package com.packtpublishing.tddjava.ch04ship;
 
 public class Ship {
 
+    private Location location;
+    public Ship(Location location) {
+        this.location = location;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
 }
--- src/test/java/com/packtpublishing/tddjava/ch04ship/ShipSpec.java
+++ src/test/java/com/packtpublishing/tddjava/ch04ship/ShipSpec.java
@@ -6,5 +6,9 @@ import static org.testng.Assert.*;
 
 @Test
 public class ShipSpec {
-
+    public void whenInitiatedThenLocationIsSet() {
+        Location location = new Location(new Point(1, 2), Direction.EAST);
+        Ship ship = new Ship(location);
+        assertEquals(location, ship.getLocation());
+    }
 }
